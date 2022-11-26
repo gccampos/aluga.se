4d098eb1991c8b1c14051f9b6cc7e29e43392980
Guilherme Campos <gcordeiro@Id.uff.br>
2022-11-25 21:31:24 -0300

    Commit com Spring Initializr

---
+++ src/main/java/com/projSotware/alugase/AlugaseApplication.java
@@ -0,0 +1,13 @@
+package com.projSotware.alugase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projSotware/alugase/AlugaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projSotware.alugase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
